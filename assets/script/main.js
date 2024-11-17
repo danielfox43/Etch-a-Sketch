664052edf252c08c677278b87efb097987ce9d97
danielfox43 <danielo.rusnaco@gmail.com>
2024-11-17 13:19:26 +0200

    First commit containing the whole porject

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,80 +1,48 @@
-let choices = document.getElementById("choices");
-let HumanImage = document.getElementById("img")
-let rock = document.getElementById("Rock")
-let paper = document.getElementById("Paper")
-let scissors = document.getElementById("Scissors")
-let ComputerImage = document.getElementById("cimg")
-console.log(rock)
-let humanScore = 0;
-let computerScore = 0;
-function getComputersChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
-    console.log(Math.floor(Math.random() * 3))
-    return choices[Math.floor(Math.random() * 3)]
-}
-choices.onclick = function(event) {
-    var choice = event.target.id;
-    PlayRound(choice, getComputersChoice());
-    
+
+const generateButton = document.querySelector("#generateButton")
+const body = document.querySelector("#screenspace")
+function GenerateGrid(y,x){
+    body.innerHTML =''
+    for (let i = 0; i < y; i++) {
+    const div = document.createElement("div");
+    div.className = "row";
+    div.style.flex = 1;
+    div.style.display = "flex";
+    body.append(div);
+   
+    for (let c = 0; c < x; c++) {
+        const divChild = document.createElement("div");
+        div.append(divChild);
+        divChild.style.opacity = 0.1
+        divChild.style.flex = 1;
     }
-function Refresh(result) {
-let result_human = document.getElementById("result_human");
-let result_computer = document.getElementById("result_computer");
-result_human.innerText = "Human: " + humanScore;
-result_computer.innerText = "Computer: " + computerScore;
-let final_result = document.getElementById("result");
-final_result.innerText = result + "!";
-}
-function HumanAnimation(type) {
-   HumanImage.style.animation = `${type} 2s ease-in-out`;
+    div.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = `rgb(${GetRandomValue()}, ${GetRandomValue()}, ${GetRandomValue()})`;
+        let opacity = parseFloat(e.target.style.opacity) 
+        if(opacity < 1) {
+            e.target.style.opacity = opacity + 0.1
 
-}
-function ComputerAnimation(type) {
-    ComputerImage.style.animation = `${type} 2s ease-in-out`;
- }
- HumanImage.addEventListener("animationstart", function(){
-    rock.disabled = true
-    paper.disabled = true
-    scissors.disabled = true
-    console.log("buttons")
-})
-HumanImage.addEventListener("animationend", function(){
-    rock.disabled = false
-    paper.disabled = false
-    scissors.disabled = false
-    ComputerImage.style.animation = `none`;
-    HumanImage.style.animation = `none`;
-})
-function PlayRound(humanChoice, computerChoice) {
-    if(computerChoice == humanChoice) {
-        Refresh("tie")
-        HumanAnimation(humanChoice.toLowerCase())
-        ComputerAnimation(computerChoice.toLowerCase())
-
-    }else if(humanChoice == "Paper" && computerChoice == "Scissors") {
-        Refresh(`${humanChoice} vs ${computerChoice}: wins BOT-${computerChoice}`)
-        HumanAnimation(humanChoice.toLowerCase())
-        ComputerAnimation(computerChoice.toLowerCase())
-        computerScore++;
-
-    }else if (humanChoice == "Rock" && computerChoice == "Paper") {
-        Refresh(`${humanChoice} vs ${computerChoice}: wins BOT-${computerChoice}`)
-        HumanAnimation(humanChoice.toLowerCase())
-        ComputerAnimation(computerChoice.toLowerCase())
-        computerScore++;
-
-    }else if (humanChoice == "Scissors" && computerChoice == "Rock"){
-        Refresh(`${humanChoice} vs ${computerChoice}: wins BOT-${computerChoice}`)
-        HumanAnimation(humanChoice.toLowerCase())
-        ComputerAnimation(computerChoice.toLowerCase())
-        computerScore++;
-
-    }else {
-        Refresh(`${humanChoice} vs ${computerChoice}: wins HUMAN-${humanChoice}`)
-        console.log("WINNER WINNER CHICKEN DINNER!")
-        HumanAnimation(humanChoice.toLowerCase())
-        ComputerAnimation(computerChoice.toLowerCase())
-        humanScore++;
-
+            
+        }
+        
+        })
+}}
+generateButton.addEventListener("click", () => {
+    let x = prompt("Enter the dimensions of the grid in the x")
+    let y = prompt("Enter the dimensions of the grid in the y")
+    if(x > 100 && y > 100) {
+        y = 100;
+        x = 100;
+        alert("Too big size set to 100 by 100")
+    }else if(x > 100) {
+        x = 100;
+        alert("Too big x size set to 100")
+    }else if(y > 100) {
+        y = 100;
+        alert("Too big y size set to 100")
     }
+    GenerateGrid(y,x)
+})
+function GetRandomValue() {
+return  Math.floor(Math.random() * 255) + 1;
 }
